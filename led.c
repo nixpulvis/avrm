95e57a5abbb30d10a59e413f348e6b218ef4dd21
Nathan Lilienthal <nathan@nixpulvis.com>
2014-08-06 13:45:32 -0400

    Minimal assembly, and C comment changes.

--- led.c
+++ led.c
@@ -7,15 +7,15 @@ enum {
 
 int main (void)
 {
-  /* set pin 5 of PORTB for output*/
+  /* set pin 5 of PORTB for output. */
   DDRB |= _BV(DDB5);
 
   while(1) {
-    /* set pin 5 high to turn led on */
+    /* set pin 5 high to turn led on. */
     PORTB |= _BV(PORTB5);
     _delay_ms(BLINK_DELAY_MS);
 
-    /* set pin 5 low to turn led off */
+    /* set pin 5 low to turn led off. */
     PORTB &= ~_BV(PORTB5);
     _delay_ms(BLINK_DELAY_MS);
   }
