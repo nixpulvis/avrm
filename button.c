c100bb2dcc063faec6426180089cd4ea170c7365
Nathan Lilienthal <nathan@nixpulvis.com>
2014-08-09 16:17:23 -0400

    Adding a use of input.

---
+++ button.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+
+enum {
+  BLINK_DELAY_MS = 1000,
+};
+
+int main (void)
+{
+  /* set pin 5 of PORTB for output. */
+  DDRB |= _BV(DDB5);
+
+  /* set pin 2 of PORTB for output. */
+  DDRB |= _BV(DDB2);
+
+  /* set pin 1 of PORTB for input. */
+  DDRB &= ~_BV(DDB1);
+
+  /* set pin 1 pull up. */
+  PORTB |= _BV(PORTB1);
+
+  while(1) {
+    /* if pin 1 is high, the button is not pressed. */
+    if (PINB & _BV(PORTB1)) {
+      /* set pin 5 low to turn led off. */
+      PORTB &= ~_BV(PORTB5);
+    } else {
+      /* set pin 5 high to turn led on. */
+      PORTB |= _BV(PORTB5);
+    }
+  }
+
+  return 0;
+}
