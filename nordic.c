4f58fd0dd6ef9b288350bdc6e9192c7de808c8a9
Nathan Lilienthal <nathan@nixpulvis.com>
2014-08-12 01:29:19 -0400

    Starting hacking on the nordic.

--- nordic.c
+++ nordic.c
@@ -9,15 +9,42 @@
 
 typedef unsigned char byte;
 
+void nRF24L01_setup(void);
+
 void main(void)
 {
   // Setup the UART, necessary for stdio actions.
   uart_begin();
 
-  char input;
-  while(1) {
-    puts("Hello world!");
-    input = getchar();
-    printf("You wrote %c\n", input);
-  }
+  // Start up the SPI bus.
+  spi_begin();
+
+  // Setup the nRF24L01.
+  nRF24L01_setup();
+}
+
+void nRF24L01_setup(void)
+{
+  // Activate command.
+  PORTB &= ~_BV(PORTB2);
+  spi_transfer(0b01010000);
+  spi_transfer(0x73);
+  PORTB |= _BV(PORTB2);
+
+  // Set CONFIG register to 00001110 (0x0E)
+  PORTB &= ~_BV(PORTB2);
+  spi_transfer(0b00100000);
+  spi_transfer(0b00001110);
+  PORTB |= _BV(PORTB2);
+
+  // Get the status of register 00000.
+  PORTB &= ~_BV(PORTB2);
+  spi_transfer(0b00000000);
+  byte status = spi_transfer(0b11111111);
+  PORTB |= _BV(PORTB2);
+
+  printf("%X\n", status);
+
+  // Pull CE low.
+  PORTB |= _BV(PORTB0);
 }
