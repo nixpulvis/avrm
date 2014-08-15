0662ec559b1e68c72b892d77d1fae87db6f29afc
Nathan Lilienthal <nathan@nixpulvis.com>
2014-08-15 14:25:30 -0400

    Sort tests.

--- nordic_test.c
+++ nordic_test.c
@@ -25,6 +25,20 @@ int main(void)
 
   printf("\nRunning Tests\n");
 
+  // Configuration Tests
+  //////////////////////
+
+  // Configure power test.
+  nRF24L01p_config_power(nRF24L01p_VALUE_CONFIG_PWR_UP);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_CONFIG);
+  assert("config_power_up", (reg & nRF24L01p_MASK_CONFIG_PWR_UP)
+                     == nRF24L01p_VALUE_CONFIG_PWR_UP);
+
+  nRF24L01p_config_power(nRF24L01p_VALUE_CONFIG_PWR_DOWN);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_CONFIG);
+  assert("config_power_down", (reg & nRF24L01p_MASK_CONFIG_PWR_UP)
+                       == nRF24L01p_VALUE_CONFIG_PWR_DOWN);
+
   // Transceiver mode test.
   nRF24L01p_config_transceiver_mode(nRF24L01p_VALUE_CONFIG_PRIM_RX);
   reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_CONFIG);
@@ -35,6 +49,12 @@ int main(void)
   reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_CONFIG);
   assert("config_transceiver_mode_tx", (reg & nRF24L01p_MASK_CONFIG_PRIM_RX)
                                        == nRF24L01p_VALUE_CONFIG_PRIM_TX);
+
+  // EN_AA
+
+  // EN_RXADDR
+
+  // SETUP_AW
 
   // Configure address width test.
   nRF24L01p_config_address_width(nRF24L01p_VALUE_SETUP_AW_AW_3);
@@ -47,19 +67,42 @@ int main(void)
   assert("config_address_width_5", (reg & nRF24L01p_MASK_SETUP_AW_AW)
                                    == nRF24L01p_VALUE_SETUP_AW_AW_5);
 
-  // Configure address test.
-  nRF24L01p_config_address(nRF24L01p_REGISTER_TX_ADDR, 0x374DFE620B);
-  reg40 = nRF24L01p_get_register40(nRF24L01p_REGISTER_TX_ADDR);
-  assert("config_address_tx", reg40 == 0x374DFE620B);
+  // SETUP_RETR
 
-  nRF24L01p_config_address(nRF24L01p_REGISTER_RX_ADDR_P0, 0x374DFE620B);
-  reg40 = nRF24L01p_get_register40(nRF24L01p_REGISTER_RX_ADDR_P0);
-  assert("config_address_rx_p0", reg40 == 0x374DFE620B);
+  // Configure retransmit delay test.
+  nRF24L01p_config_retransmit_delay(nRF24L01p_VALUE_SETUP_RETR_ARD_750);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
+  assert("config_retransmit_delay_750", (reg & nRF24L01p_MASK_SETUP_RETR_ARD)
+                                      == nRF24L01p_VALUE_SETUP_RETR_ARD_750);
 
-  // Configure payload width test.
-  nRF24L01p_config_payload_width(nRF24L01p_REGISTER_RX_PW_P0, 17);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_RX_PW_P0);
-  assert("config_payload_width", (reg & nRF24L01p_MASK_RX_PW) == 17);
+  nRF24L01p_config_retransmit_delay(nRF24L01p_VALUE_SETUP_RETR_ARD_4000);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
+  assert("config_retransmit_delay_15", (reg & nRF24L01p_MASK_SETUP_RETR_ARD)
+                                      == nRF24L01p_VALUE_SETUP_RETR_ARD_4000);
+
+  // Configure retransmit count test.
+  nRF24L01p_config_retransmit_count(nRF24L01p_VALUE_SETUP_RETR_ARC_0);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
+  assert("config_retransmit_count_0", (reg & nRF24L01p_MASK_SETUP_RETR_ARC)
+                                      == nRF24L01p_VALUE_SETUP_RETR_ARC_0);
+
+  nRF24L01p_config_retransmit_count(nRF24L01p_VALUE_SETUP_RETR_ARC_15);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
+  assert("config_retransmit_count_15", (reg & nRF24L01p_MASK_SETUP_RETR_ARC)
+                                      == nRF24L01p_VALUE_SETUP_RETR_ARC_15);
+
+  // RF_CH
+
+  // Configure channel test.
+  nRF24L01p_config_channel(0);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_RF_CH);
+  assert("config_channel_0", (reg & nRF24L01p_MASK_RF_CH_RF_CH) == 0);
+
+  nRF24L01p_config_channel(97);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_RF_CH);
+  assert("config_channel_97", (reg & nRF24L01p_MASK_RF_CH_RF_CH) == 97);
+
+  // RF_SETUP
 
   // Configure air data rate test.
   nRF24L01p_config_air_data_rate(nRF24L01p_VALUE_RF_SETUP_RF_DR_1Mbps);
@@ -83,47 +126,28 @@ int main(void)
   assert("config_output_power_0dBm", (reg & nRF24L01p_MASK_RF_SETUP_RF_PWR)
                                      == nRF24L01p_VALUE_RF_SETUP_RF_PWR_0dBm);
 
-  // Configure power test.
-  nRF24L01p_config_power(nRF24L01p_VALUE_CONFIG_PWR_UP);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_CONFIG);
-  assert("config_power_up", (reg & nRF24L01p_MASK_CONFIG_PWR_UP)
-                     == nRF24L01p_VALUE_CONFIG_PWR_UP);
+  // CD
 
-  nRF24L01p_config_power(nRF24L01p_VALUE_CONFIG_PWR_DOWN);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_CONFIG);
-  assert("config_power_down", (reg & nRF24L01p_MASK_CONFIG_PWR_UP)
-                       == nRF24L01p_VALUE_CONFIG_PWR_DOWN);
+  // RX_ADDR_P(0-5) and TX_ADDR
 
-  // Configure retransmit count test.
-  nRF24L01p_config_retransmit_count(nRF24L01p_VALUE_SETUP_RETR_ARC_0);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
-  assert("config_retransmit_count_0", (reg & nRF24L01p_MASK_SETUP_RETR_ARC)
-                                      == nRF24L01p_VALUE_SETUP_RETR_ARC_0);
+  // Configure address test.
+  nRF24L01p_config_address(nRF24L01p_REGISTER_TX_ADDR, 0x374DFE620B);
+  reg40 = nRF24L01p_get_register40(nRF24L01p_REGISTER_TX_ADDR);
+  assert("config_address_tx", reg40 == 0x374DFE620B);
 
-  nRF24L01p_config_retransmit_count(nRF24L01p_VALUE_SETUP_RETR_ARC_15);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
-  assert("config_retransmit_count_15", (reg & nRF24L01p_MASK_SETUP_RETR_ARC)
-                                      == nRF24L01p_VALUE_SETUP_RETR_ARC_15);
+  nRF24L01p_config_address(nRF24L01p_REGISTER_RX_ADDR_P0, 0x374DFE620B);
+  reg40 = nRF24L01p_get_register40(nRF24L01p_REGISTER_RX_ADDR_P0);
+  assert("config_address_rx_p0", reg40 == 0x374DFE620B);
 
-  // Configure retransmit delay test.
-  nRF24L01p_config_retransmit_delay(nRF24L01p_VALUE_SETUP_RETR_ARD_750);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
-  assert("config_retransmit_delay_750", (reg & nRF24L01p_MASK_SETUP_RETR_ARD)
-                                      == nRF24L01p_VALUE_SETUP_RETR_ARD_750);
+  // RX_PW_P(0-5)
 
-  nRF24L01p_config_retransmit_delay(nRF24L01p_VALUE_SETUP_RETR_ARD_4000);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_SETUP_RETR);
-  assert("config_retransmit_delay_15", (reg & nRF24L01p_MASK_SETUP_RETR_ARD)
-                                      == nRF24L01p_VALUE_SETUP_RETR_ARD_4000);
+  // Configure payload width test.
+  nRF24L01p_config_payload_width(nRF24L01p_REGISTER_RX_PW_P0, 17);
+  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_RX_PW_P0);
+  assert("config_payload_width", (reg & nRF24L01p_MASK_RX_PW) == 17);
 
-  // Configure channel test.
-  nRF24L01p_config_channel(0);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_RF_CH);
-  assert("config_channel_0", (reg & nRF24L01p_MASK_RF_CH_RF_CH) == 0);
-
-  nRF24L01p_config_channel(97);
-  reg = nRF24L01p_get_register8(nRF24L01p_REGISTER_RF_CH);
-  assert("config_channel_97", (reg & nRF24L01p_MASK_RF_CH_RF_CH) == 97);
+  // FIFO STATUS TESTS
+  ////////////////////
 
   // FIFO status tests.
   spi_start();
