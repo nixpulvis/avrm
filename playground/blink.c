827df61407cb7f5c040fdc9937d1d0a84e14490c
Nathan Lilienthal <nathan@nixpulvis.com>
2018-01-19 09:11:27 -0700

    Update blink example to use pin functions.

--- playground/blink.c
+++ playground/blink.c
@@ -1,20 +1,17 @@
-/* blink.c
- *
- * Flash the mini built in LED on the avrm test board.
- */
-#include <avr/io.h>
-#include <util/delay.h>
+#include <avrm.h>
+#include <avrm/pin.h>
 
-int main(void)
-{
-  DDRB |= _BV(DDB5);
+#define LED_PIN 13
 
-  while(1) {
-    PORTB |= _BV(DDB5);
-    _delay_ms(250);
-    PORTB &= ~_BV(DDB5);
-    _delay_ms(250);
-  }
+int main(void) {
+    pin_mode(LED_PIN, OUTPUT);
 
-  return 0;
+    for (;;) {
+        pin_out(LED_PIN, TRUE);
+        delay_ms(250);
+        pin_out(LED_PIN, FALSE);
+        delay_ms(250);
+    }
+
+    return 0;
 }
