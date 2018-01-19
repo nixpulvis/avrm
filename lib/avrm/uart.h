e44e6e2202c5965111e7671c9cea6e9bf1a32d64
Nathan Lilienthal <nathan@nixpulvis.com>
2018-01-19 11:53:52 -0700

    Add an example in the docs for uart_init.

--- lib/avrm/uart.h
+++ lib/avrm/uart.h
@@ -10,10 +10,19 @@
 
 #include "../avrm.h"
 
-// uart_init
-// Initialize the UART bus. The bus is bound to STDIN and STDOUT, so
-// access to the UART is done through the standard stdio functions.
-//
+/// uart_init
+/// Initialize the UART bus. The bus is bound to STDIN and STDOUT, so
+/// access to the UART is done through the standard stdio functions.
+///
+/// # Example
+///
+/// ```
+/// // Setupt the UART.
+/// uart_init();
+///
+/// // Print something over the serial UART.
+/// printf("ping\n");
+/// ```
 void uart_init(void);
 
 #endif
