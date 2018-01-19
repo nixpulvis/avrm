fe251a40c199c938c4c2199abb8f8aad3bb47502
Nathan Lilienthal <nathan@nixpulvis.com>
2018-01-18 21:23:25 -0700

    Add the pin_out function.

--- lib/avrm/port.c
+++ lib/avrm/port.c
@@ -1,2 +1,18 @@
-#include "i2c.h"
+#include "port.h"
+
+void pin_out(int pin, bool value) {
+    if (pin <= 7) {
+        if (value) {
+            PORTD |= _BV(pin);
+        } else {
+            PORTD &= ~_BV(pin);
+        }
+    } else if (pin <= 13) {
+        if (value) {
+            PORTB |= _BV(pin - 8);
+        } else {
+            PORTB &= ~_BV(pin - 8);
+        }
+    }
+}
 
