a5946374a75c4390e7e3ed2e98e94630c2a371bb
Nathan Lilienthal <nathan@nixpulvis.com>
2018-01-18 23:40:19 -0700

    Fix shift.h's avrm import.

--- lib/avrm/shift.h
+++ lib/avrm/shift.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 #include <avr/io.h>
-#include <avrm.h>
-#include <avrm/pin.h>
+#include "../avrm.h"
+#include "../avrm/pin.h"
 
 void shift(int data_pin, int clock_pin, uint8_t n);
 void shift_latch(int data_pin, int clock_pin, int latch_pin, uint8_t n);
